4f16b70975a605330ed3103385f977ebf1c3af3a
VeselovTimofey <vestimofey@mail.ru>
2022-08-03 14:59:31 +0300

    docker

--- rekruto/rekruto/asgi.py
+++ rekruto/rekruto/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Recruto project.
+ASGI config for rekruto project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Recruto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rekruto.settings')
 
 application = get_asgi_application()
--- rekruto/rekruto/wsgi.py
+++ rekruto/rekruto/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Recruto project.
+WSGI config for rekruto project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Recruto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rekruto.settings')
 
 application = get_wsgi_application()
